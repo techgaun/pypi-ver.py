626a19001aa5463c532bf836762fefd04803a50f
techgaun <samar+github@techgaun.com>
2016-09-11 15:25:48 -0500

    update for 2/3 compatibility

--- pypi-ver.py
+++ pypi-ver.py
@@ -12,13 +12,13 @@ def get_versions(pkg_name):
     response = requests.get(url)
     if response.status_code == requests.codes.ok:
         data = response.json()
-        versions = data["releases"].keys()
+        versions = list(data["releases"].keys())
         versions.sort(key=LooseVersion, reverse=True)
     else:
         versions = ["No such package found"]
     return versions
 
 if len(sys.argv) == 1:
-    print "Usage: %s <package_name>" % (sys.argv[0])
+    print("Usage: {} <package_name>".format(sys.argv[0]))
     sys.exit(1)
-print "\n".join(get_versions(sys.argv[1]))
+print("\n".join(get_versions(sys.argv[1])))
